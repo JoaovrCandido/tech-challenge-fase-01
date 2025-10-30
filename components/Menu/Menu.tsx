a990c50b68214efe6f4e7850cf4618b73a24a72e
fefe <20fer00@gmail.com>
2025-10-30 20:35:55 -0300

    Menu responsivo

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,52 +1,73 @@
 "use client";
 import { useState } from "react";
-import style from "./Menu.module.css"
-import Link from 'next/link'
+import style from "./Menu.module.css";
+import Link from "next/link";
 import { useIsMobile } from "@/hooks/useIsMobile";
 import { usePathname } from "next/navigation";
+
 export default function Menu() {
-    const [open, setOpen] = useState(false);
-    const isMobile = useIsMobile();
-    const pathname = usePathname();
+  const [open, setOpen] = useState(false);
+  const isMobile = useIsMobile();
+  const pathname = usePathname();
 
-    return (
-        <div className={style.Menu}>
+  // função pra fechar o menu
+  const handleLinkClick = () => {
+    if (isMobile) setOpen(false);
+  };
 
-            {isMobile ? (
-                <>
-                    <ul className={`${style.menuLinks} ${open ? style.active : style.inactive}`}>
-                        <li
-                            className={`${style.menuItem} ${pathname === "/" ? style.activeItem : ""}`}
-                        >
-                            <Link href="/">Início</Link>
-                        </li>
+  return (
+    <div className={style.Menu}>
+      {isMobile ? (
+        <>
+          <button className={style.btnToggle} onClick={() => setOpen(!open)}>
+            ☰
+          </button>
 
-                        <li
-                            className={`${style.menuItem} ${pathname === "/transacoes" ? style.activeItem : ""}`}
-                        >
-                            <Link href="/transacoes">Transações</Link>
-                        </li>
-                    </ul>
+          <ul
+            className={`${style.menuLinks} ${
+              open ? style.active : style.inactive
+            }`}
+          >
+            <li
+              className={`${style.menuItem} ${
+                pathname === "/" ? style.activeItem : ""
+              }`}
+            >
+              <Link href="/" onClick={handleLinkClick}>
+                Início
+              </Link>
+            </li>
 
-                    <button className={style.btnToggle} onClick={() => setOpen(!open)}>
-                        ☰
-                    </button>
-                </>
-            ) : (
-                <ul className={style.menuLinks}>
-                    <li
-                        className={`${style.menuItem} ${pathname === "/" ? style.activeItem : ""}`}
-                    >
-                        <Link href="/">Início</Link>
-                    </li>
+            <li
+              className={`${style.menuItem} ${
+                pathname === "/transacoes" ? style.activeItem : ""
+              }`}
+            >
+              <Link href="/transacoes" onClick={handleLinkClick}>
+                Transações
+              </Link>
+            </li>
+          </ul>
+        </>
+      ) : (
+        <ul className={style.menuLinks}>
+          <li
+            className={`${style.menuItem} ${
+              pathname === "/" ? style.activeItem : ""
+            }`}
+          >
+            <Link href="/">Início</Link>
+          </li>
 
-                    <li
-                        className={`${style.menuItem} ${pathname === "/transacoes" ? style.activeItem : ""}`}
-                    >
-                        <Link href="/transacoes">Transações</Link>
-                    </li>
-                </ul>
-            )}
-        </div>
-    );
+          <li
+            className={`${style.menuItem} ${
+              pathname === "/transacoes" ? style.activeItem : ""
+            }`}
+          >
+            <Link href="/transacoes">Transações</Link>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
 }
